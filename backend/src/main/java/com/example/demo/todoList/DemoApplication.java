9d9d62cfb984c1ac151132196045b2334386f521
Steve Dölgner <steve.doelgner@gmail.com>
2022-02-19 10:30:27 +0100

    mappings implemented

--- backend/src/main/java/com/example/demo/todoList/DemoApplication.java
+++ backend/src/main/java/com/example/demo/todoList/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.todoList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/main/java/com/example/demo/todoList/TodoController.java
+++ backend/src/main/java/com/example/demo/todoList/TodoController.java
@@ -1,8 +1,6 @@
 package com.example.demo.todoList;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,9 +14,29 @@ public class TodoController {
         this.todoService = todoService;
     }
 
+    @PostMapping
+    public void addItem(@RequestBody TodoItem todoItem) {
+        todoService.addItem(todoItem);
+    }
+
+    @DeleteMapping ("/{idItem}")
+    public void deleteItem(@PathVariable String idItem) {
+        todoService.deleteItem(idItem);
+    }
+
+    @PutMapping("/{idItem}")
+    public void setStatusDone(@PathVariable String idItem) {
+        todoService.setStatusDone(idItem);
+    }
+
     @GetMapping("/listAllItem")
     public List<TodoItem> listAllItem() {
         return todoService.listAllItem();
+    }
+
+    @GetMapping("{idItem}")
+    public TodoItem listOneItem(@PathVariable String idItem) {
+        return todoService.listOneItem(idItem);
     }
 
 }
