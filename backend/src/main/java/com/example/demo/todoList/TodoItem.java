6326d18d4c4a6a23ce7dc336fa01257953f55303
Steve Dölgner <steve.doelgner@gmail.com>
2022-02-21 19:32:01 +0100

    start with lombok

--- backend/src/main/java/com/example/demo/todoList/TodoItem.java
+++ backend/src/main/java/com/example/demo/todoList/TodoItem.java
@@ -1,54 +1,16 @@
 package com.example.demo.todoList;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
+@Data
+@NoArgsConstructor
 public class TodoItem {
 
     private String id;
     private String content;
     private boolean statusDone;
-
-    public TodoItem() {
-        this("");
-    }
-
-    public TodoItem(String content) {
-        this(UUID.randomUUID().toString(), content, false);
-    }
-
-    public TodoItem(String id, String content, boolean statusDone) {
-        this.id = id;
-        this.content = content;
-        this.statusDone = statusDone;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public boolean isStatusDone() {
-        return statusDone;
-    }
-
-    public void setStatusDone(boolean statusDone) {
-        this.statusDone = statusDone;
-    }
-
-    @Override
-    public String toString() {
-        return content;
-    }
 
 }
